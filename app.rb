95710173768c28171f343e8a9e0f251dd6342b12
tsunufu <tsunufu@icloud.com>
2022-02-23 12:12:07 +0000

    ログイン機能途中まで

--- app.rb
+++ app.rb
@@ -4,6 +4,34 @@ require 'sinatra/reloader' if development?
 require './models'
 require 'dotenv/load'
 
+enable :sessions
+
 get '/' do
     erb :index
+end
+
+get '/signup' do
+    erb :sign_up
+end
+
+post '/signin' do
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        session[:user] = user.id
+    end
+    redirect '/'
+end
+
+post '/signup' do
+    @user = User.create(name:params[:name], password:params[:password],
+    password_confirmation:params[:password_confirmation])
+    if @user.persisted?
+        session[:user] = @user.id
+    end
+    redirect '/'
+end
+
+get '/signout' do
+    session[:user] = nil
+    redirect '/'
 end
--- models.rb
+++ models.rb
@@ -2,3 +2,7 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection
+
+class User < ActiveRecord::Base
+    has_secure_password
+end
