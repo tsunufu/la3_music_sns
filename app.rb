a539a8aeb0f504e319a5388b4abbeeaeb2a8f5cd
tsunufu <tsunufu@icloud.com>
2022-02-25 06:52:07 +0000

    写真うまくいかない

--- app.rb
+++ app.rb
@@ -6,12 +6,29 @@ require 'dotenv/load'
 
 enable :sessions
 
+before do
+    Dotenv.load
+    Cloudinary.config do |config|
+        config.cloud_name = ENV['CLOUD_NAME']
+        config.api_key = ENV['CLOUDINARY_API_KEY']
+        config.api_secret = ENV['CLOUDINARY_API_SECRET']
+    end
+end
+
 get '/' do
     erb :index
 end
 
 get '/signup' do
     erb :sign_up
+end
+
+get '/home' do
+    erb :home
+end
+
+get '/search' do
+    erb :search
 end
 
 post '/signin' do
@@ -23,8 +40,18 @@ post '/signin' do
 end
 
 post '/signup' do
+    img_url = ''
+    if params[:file]
+        img = params[:file]
+        tempfile = img[:tempfile]
+        upload = Cloudinary::Uploader.upload(tempfile.path)
+        img_url = upload['url']
+    end
+    
     @user = User.create(name:params[:name], password:params[:password],
-    password_confirmation:params[:password_confirmation])
+    password_confirmation:params[:password_confirmation],
+    icon: img_url
+    )
     if @user.persisted?
         session[:user] = @user.id
     end
