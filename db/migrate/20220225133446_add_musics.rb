51902221e1353f7b9c5f3ece24a2e19769586c3e
tsunufu <tsunufu@icloud.com>
2022-03-21 08:04:50 +0000

    マイグレートできない問題を解消ver.2

--- db/migrate/20220225133446_add_musics.rb
+++ db/migrate/20220225133446_add_musics.rb
@@ -1,11 +1,11 @@
 class AddMusics < ActiveRecord::Migration[6.1]
-  #def change
-  #add_timestamps :musics, null: true 
+  def change
+    add_timestamps :products, null: true 
 
-  #long_ago = DateTime.new(2000, 1, 1)
-  #Music.update_all(created_at: long_ago, updated_at: long_ago)
+    long_ago = DateTime.new(2000, 1, 1)
+    Product.update_all(created_at: long_ago, updated_at: long_ago)
 
-  #change_column_null :musics, :created_at, false
-  #change_column_null :musics, :updated_at, false
-  #end
+    change_column_null :products, :created_at, false
+    change_column_null :products, :updated_at, false
+  end
 end
